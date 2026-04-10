57568d76ed954bf6242004bb1c1950a06acbc888
pasha <hanwoung@naver.com>
2026-04-10 11:16:46 +0900

    Code-only sync from T_Proto

---
+++ Plugins/HelpfulFunctions/Source/HelpfulFunctions/Private/AsyncLoadAssetsNode.cpp
@@ -0,0 +1,85 @@
+
+
+
+#include "AsyncLoadAssetsNode.h"
+#include "Engine/AssetManager.h"
+
+UAsyncLoadAssetsNode::UAsyncLoadAssetsNode()
+{
+}
+
+UAsyncLoadAssetsNode* UAsyncLoadAssetsNode::AsyncLoadAssetArray(const TArray<TSoftObjectPtr<UObject>>& AssetsToLoad)
+{
+    UAsyncLoadAssetsNode* Node = NewObject<UAsyncLoadAssetsNode>();
+    Node->Assets = AssetsToLoad;
+    return Node;
+}
+
+void UAsyncLoadAssetsNode::Activate()
+{
+    Assets.RemoveAll([](const TSoftObjectPtr<UObject>& Asset)
+        {
+            return Asset.IsNull() || !Asset.ToSoftObjectPath().IsValid();
+        });
+
+    if (Assets.Num() == 0)
+    {
+        // Jeśli lista zasobów jest pusta, natychmiast zakończ
+        OnCompleted.Broadcast(LoadedAssets);
+        return;
+    }
+
+    LoadNextAsset();
+}
+
+void UAsyncLoadAssetsNode::LoadNextAsset()
+{
+    if (Assets.Num() == 0)
+    {
+        // Wszystkie zasoby zostały załadowane
+        OnCompleted.Broadcast(LoadedAssets);
+        return;
+    }
+
+    // Pobierz pierwszy zasób z listy
+    TSoftObjectPtr<UObject> AssetToLoad = Assets[0];
+    Assets.RemoveAt(0);
+
+    if (AssetToLoad.IsValid())
+    {
+        // Jeśli zasób jest już załadowany, dodaj go do wyników
+        LoadedAssets.Add(AssetToLoad.Get());
+        LoadNextAsset();
+    }
+    else
+    {
+        const FSoftObjectPath AssetPath = AssetToLoad.ToSoftObjectPath();
+        if (!AssetPath.IsValid())
+        {
+            LoadNextAsset();
+            return;
+        }
+
+        // Asynchronicznie załaduj zasób i wywołaj callback po zakończeniu
+        StreamableManager.RequestAsyncLoad(
+            AssetPath,
+            FStreamableDelegate::CreateUObject(this, &UAsyncLoadAssetsNode::OnAssetLoaded, AssetToLoad)
+        );
+    }
+}
+
+void UAsyncLoadAssetsNode::OnAssetLoaded(TSoftObjectPtr<UObject> LoadedAsset)
+{
+    if (LoadedAsset.IsValid())
+    {
+        UObject* LoadedObject = LoadedAsset.Get();
+        if (LoadedObject)
+        {
+            LoadedAssets.Add(LoadedObject);
+        }
+    }
+
+    // Przejdź do następnego zasobu
+    LoadNextAsset();
+}
+
